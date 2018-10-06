bbb4fb8541d84af570483275442c19c4d28f99b8
testxxc <naivesen@gmail.com>
2018-10-06 12:34:13 +0800

    add build shell

--- wcf/src/wcf/wcf_local_test.go
+++ wcf/src/wcf/wcf_local_test.go
@@ -4,12 +4,21 @@ import (
 	"testing"
 	"time"
 	"github.com/sirupsen/logrus"
+	"net"
 )
+
+func TestConnect(t *testing.T) {
+	conn, err := net.DialTimeout("tcp", "www.pin-cong.com:443", 2 * time.Second)
+	if err != nil {
+		t.Fatal(err)
+	}
+	conn.Close()
+}
 
 func TestStart(t *testing.T) {
 	cfg := LocalConfig{}
 	cfg.Timeout = 5 * time.Second
-	cfg.Proxyaddr = "127.0.0.1:8020"
+	cfg.Proxyaddr = []ProxyAddrInfo {ProxyAddrInfo{"127.0.0.1:8020", 50}}
 	cfg.Localaddr = append(cfg.Localaddr, AddrConfig{Name:"socks", Address:"127.0.0.1:8010"})
 	cfg.User = "test"
 	cfg.Pwd = "xxx"
